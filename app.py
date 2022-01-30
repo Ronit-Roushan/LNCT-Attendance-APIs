abf591e752f17d657a4bddf330ab682b41ed0fbe
Ronit Roushan <ronitroushan9546@gmail.com>
2022-01-30 20:43:32 +0530

    02

--- app.py
+++ app.py
@@ -11,24 +11,40 @@ def landing():
 
 @app.route("/datewise-attaindance-<int:username>-<string:password>")
 def datewise(username, password):
-    if(username == 'username' and password == 'password'):
-        return 'Update username and password..'
+    if(username == 123456 and password == '123456'):
+        return jsonify([{
+                    'sno.': 0,
+                    'date': 0,
+                    'period': 0,
+                    'subject': 0,
+                    'Attend Status': 0
+        }])
     else:
         scrapper = dwaScrapper()
         return jsonify(scrapper.scrap(username=username, password=password))
 
 @app.route("/subjectwise-attaindance-<int:username>-<string:password>")
 def subjectwise(username, password):
-    if(username == 'username' and password == 'password'):
-        return 'Update username and password..'
+    if(username == 123456 and password == '123456'):
+        return jsonify([{
+                    'subject': 0,
+                    'subject_code': 0,
+                    'total lectures': 0,
+                    'present': 0
+        }])
     else:
         scrapper = swScrapper()
         return jsonify(scrapper.scrap(username=username, password=password))
 
 @app.route("/overall-attaindance-<int:username>-<string:password>")
 def overall(username, password):
-    if(username == 'username' and password == 'password'):
-        return 'Update username and password..'
+    if(username == 123456 and password == '123456'):
+        return jsonify([{
+                'Total Classes': 0,
+                'Present': 0,
+                'Percentage': 0,
+                'Absent': 0
+        }])
     else:
         scrapper = oaScrapper()
         return jsonify(scrapper.scrap(username=username, password=password))
