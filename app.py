adb6a04291e4f81e2ec8609be5f7fd8392784e8f
Ronit Roushan <ronitroushan9546@gmail.com>
2022-01-30 22:14:51 +0530

    04 Working Fine

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def landing():
     return render_template('index.html')
 
-@app.route("/datewise-attaindance-<int:username>-<string:password>")
+@app.route("/datewise-attendance-<int:username>-<string:password>")
 def datewise(username, password):
     if(username == 123456 and password == '123456'):
         return jsonify([{
@@ -23,7 +23,7 @@ def datewise(username, password):
         scrapper = dwaScrapper()
         return jsonify(scrapper.scrap(username=username, password=password))
 
-@app.route("/subjectwise-attaindance-<int:username>-<string:password>")
+@app.route("/subjectwise-attendance-<int:username>-<string:password>")
 def subjectwise(username, password):
     if(username == 123456 and password == '123456'):
         return jsonify([{
@@ -36,7 +36,7 @@ def subjectwise(username, password):
         scrapper = swScrapper()
         return jsonify(scrapper.scrap(username=username, password=password))
 
-@app.route("/overall-attaindance-<int:username>-<string:password>")
+@app.route("/overall-attendance-<int:username>-<string:password>")
 def overall(username, password):
     if(username == 123456 and password == '123456'):
         return jsonify([{
